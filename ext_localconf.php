a596b3d43c62c0c14c55750e0af2d9c9dc7604bb
Andy Meier <andy.meier@visol.ch>
2025-07-29 10:26:36 +0200

    chore: include addiional be stylesheet

---
+++ ext_localconf.php
@@ -0,0 +1,7 @@
+<?php
+
+if (!defined('TYPO3')) {
+    die('Access denied.');
+}
+
+$GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets']['treehighlight'] = 'EXT:treehighlight/Resources/Public/Stylesheets/Backend/pageTree.css';
