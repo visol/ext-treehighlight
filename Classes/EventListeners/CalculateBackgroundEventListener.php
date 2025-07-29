b71c31b2f4564e1e63764bf9334a59b221b69c7c
Andy Meier <andy.meier@visol.ch>
2025-07-29 10:24:20 +0200

    chore: add more meaningfull tool-tip label, set priority:1

--- Classes/EventListeners/CalculateBackgroundEventListener.php
+++ Classes/EventListeners/CalculateBackgroundEventListener.php
@@ -8,6 +8,7 @@ use TYPO3\CMS\Backend\Dto\Tree\Label\Label;
 use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Localization\LanguageService;
 
 class CalculateBackgroundEventListener
 {
@@ -23,9 +24,20 @@ class CalculateBackgroundEventListener
             if (in_array($page['perms_groupid'], $backendUserGroups) || (int)$page['perms_userid'] === $this->getBackendUser()->user['uid']) {
                 // user has access by group permissions or is the owner of the page
                 $item = $event->getItem();
+
+                $labelTitle = $this->getLanguageService()->translateLabel(
+                    input: 'LLL:EXT:beuser/Resources/Private/Language/locallang_mod_permission.xlf:mlang_labels_tablabel',
+                    fallback: 'Edit Page Permissions'
+                );
+                $labelGranted = $this->getLanguageService()->translateLabel(
+                    input: 'LLL:EXT:beuser/Resources/Private/Language/locallang_mod_permission.xlf:A_Granted',
+                    fallback: 'Access granted'
+                );
+
                 $item['labels'][] = new Label(
-                    label: $item['name'],
+                    label: $labelTitle . ': ' . $labelGranted,
                     color: $this->getBackgroundColor(),
+                    priority: 1
                 );
 
                 $event->setItem($item);
@@ -43,4 +55,10 @@ class CalculateBackgroundEventListener
         $extensionConfiguration = GeneralUtility::makeInstance(ExtensionConfiguration::class);
         return (string)$extensionConfiguration->get('treehighlight', 'backgroundColor');
     }
+
+    protected function getLanguageService(): LanguageService
+    {
+        return $GLOBALS['LANG'];
+    }
+
 }
